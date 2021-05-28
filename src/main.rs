531b00f69ff70e3a79035989614e00b35f641c34
starliiit <esu.eru.sl@gmail.com>
2021-05-28 18:56:37 +0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 mod protocols;
 
+use pcap_parser::traits::PcapReaderIterator;
+use pcap_parser::{LegacyPcapReader, PcapBlockOwned, PcapError};
+use std::fs::File;
+
+use protocols::{parse_packet, Packet};
+
 fn main() {
-    println!("Hello, world!");
+    let path = "/home/sl/pcap/ICS/modbus/mod_3.pcap";
+    let file = File::open(path).unwrap();
+    let mut num_blocks = 0;
+    let mut reader = LegacyPcapReader::new(65536, file).unwrap();
+    loop {
+        match reader.next() {
+            Ok((offset, block)) => {
+                println!("got new block");
+                num_blocks += 1;
+                match block {
+                    PcapBlockOwned::LegacyHeader(_hdr) => {
+                        println!("{:?}", _hdr);
+                        // save hdr.network (linktype)
+                    }
+                    PcapBlockOwned::Legacy(_b) => {
+                        // use linktype to parse b.data()
+                        println!("{:?}", _b);
+                    }
+                    PcapBlockOwned::NG(_) => unreachable!(),
+                }
+                reader.consume(offset);
+            }
+            Err(PcapError::Eof) => break,
+            Err(PcapError::Incomplete) => {
+                reader.refill().unwrap();
+            }
+            Err(e) => panic!("error while reading: {:?}", e),
+        }
+    }
+    println!("number of blocks: {:?}", num_blocks);
 }
