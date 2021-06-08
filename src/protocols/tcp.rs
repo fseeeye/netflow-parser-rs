b2a49fb14111758736a99b0d2c622b01c02ec8e4
sl <dongshouliu@bolean.com.cn>
2021-06-08 14:31:01 +0800

    tcp refactor

--- src/protocols/tcp.rs
+++ src/protocols/tcp.rs
@@ -24,13 +24,7 @@ pub struct Tcp<'a> {
     pub options: Option<&'a [u8]>,
 }
 
-// #[derive(Debug, PartialEq)]
-// pub struct Tcp<'a> {
-//     pub header: TcpHeader,
-//     pub options: Option<&'a [u8]>,
-// }
-
-fn parse_tcp(input: &[u8]) -> IResult<&[u8], Tcp> {
+pub fn parse_tcp(input: &[u8]) -> IResult<&[u8], Tcp> {
     let (input, src_port) = be_u16(input)?;
     let (input, dst_port) = be_u16(input)?;
     let (input, seq) = be_u32(input)?;
@@ -50,17 +44,6 @@ fn parse_tcp(input: &[u8]) -> IResult<&[u8], Tcp> {
     } else {
         Ok((input, None))
     }?;
-
-    // let (input, src_port) = take_bits(16usize)(input)?;
-    // let (input, dst_port) = take_bits(16usize)(input)?;
-    // let (input, seq) = take_bits(32usize)(input)?;
-    // let (input, ack) = take_bits(32usize)(input)?;
-    // let (input, header_length) = take_bits(4usize)(input)?;
-    // let (input, reserved) = take_bits(3usize)(input)?;
-    // let (input, flags) = take_bits(9usize)(input)?;
-    // let (input, window_size) = take_bits(16usize)(input)?;
-    // let (input, checksum) = take_bits(16usize)(input)?;
-    // let (input, urgent_pointer) = take_bits(16usize)(input)?;
     Ok((
         input,
         Tcp {
@@ -78,6 +61,76 @@ fn parse_tcp(input: &[u8]) -> IResult<&[u8], Tcp> {
         },
     ))
 }
+
+// #[derive(Debug, PartialEq)]
+// pub struct Tcp<'a> {
+//     pub src_port: u16,
+//     pub dst_port: u16,
+//     pub seq: u32,
+//     pub ack: u32,
+//     pub header_length: u8,
+//     pub reserved: u8,
+//     pub flags: u16,
+//     pub window_size: u16,
+//     pub checksum: u16,
+//     pub urgent_pointer: u16,
+//     pub options: Option<&'a [u8]>,
+// }
+
+// // #[derive(Debug, PartialEq)]
+// // pub struct Tcp<'a> {
+// //     pub header: TcpHeader,
+// //     pub options: Option<&'a [u8]>,
+// // }
+
+// fn parse_tcp(input: &[u8]) -> IResult<&[u8], Tcp> {
+//     let (input, src_port) = be_u16(input)?;
+//     let (input, dst_port) = be_u16(input)?;
+//     let (input, seq) = be_u32(input)?;
+//     let (input, ack) = be_u32(input)?;
+//     let (input, (header_length, reserved, flags)) =
+//         bits::<_, _, nom::error::Error<(&[u8], usize)>, _, _>(tuple((
+//             take_bits(4usize),
+//             take_bits(3usize),
+//             take_bits(9usize),
+//         )))(input)?;
+//     let (input, window_size) = be_u16(input)?;
+//     let (input, checksum) = be_u16(input)?;
+//     let (input, urgent_pointer) = be_u16(input)?;
+//     let (input, options) = if (header_length * 4) > 20 {
+//         let (input, options) = take(header_length * 4 - 20)(input)?;
+//         Ok((input, Some(options)))
+//     } else {
+//         Ok((input, None))
+//     }?;
+
+//     // let (input, src_port) = take_bits(16usize)(input)?;
+//     // let (input, dst_port) = take_bits(16usize)(input)?;
+//     // let (input, seq) = take_bits(32usize)(input)?;
+//     // let (input, ack) = take_bits(32usize)(input)?;
+//     // let (input, header_length) = take_bits(4usize)(input)?;
+//     // let (input, reserved) = take_bits(3usize)(input)?;
+//     // let (input, flags) = take_bits(9usize)(input)?;
+//     // let (input, window_size) = take_bits(16usize)(input)?;
+//     // let (input, checksum) = take_bits(16usize)(input)?;
+//     // let (input, urgent_pointer) = take_bits(16usize)(input)?;
+//     Ok((
+//         input,
+//         Tcp {
+//             src_port,
+//             dst_port,
+//             seq,
+//             ack,
+//             header_length,
+//             reserved,
+//             flags,
+//             window_size,
+//             checksum,
+//             urgent_pointer,
+//             options,
+//         },
+//     ))
+// }
 
 #[derive(Debug, PartialEq)]
 pub struct Packet<'a> {
