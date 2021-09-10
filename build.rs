f15c4cbbc1beff95190dcc71bc539151c7976b0d
fseeeye <liyifan@bolean.com.cn>
2021-09-10 15:48:41 +0800

    chore(cbindgen): add necessary requires of FFI

---
+++ build.rs
@@ -0,0 +1,14 @@
+extern crate cbindgen;
+
+use std::{env, path::PathBuf};
+
+fn main() {
+	let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let bindings = cbindgen::generate(crate_dir)
+    	.expect("Unable to generate bindings");
+    
+	let out_path = PathBuf::from("./target/debug/");
+    bindings.write_to_file(out_path.join("bindings.h"));
+	// bindings.write_to_file(format!("target/debug/{}.h", env::var("CARGO_PKG_NAME").unwrap().replace("-", "_")));
+}
