36f09d16d96dd2e1e03bb8aee7f86c0a9557f44d
fseeeye <liyifan@bolean.com.cn>
2021-11-11 21:09:22 +0800

    chore(adapter): change output name

--- crates/adapter_clang/build.rs
+++ crates/adapter_clang/build.rs
@@ -11,5 +11,5 @@ fn main() {
 	let out_path = PathBuf::from("./output/"); // 在此修改头文件输出路径
 	
 	// bindings.write_to_file(out_path.join(format!("{}.h", env::var("CARGO_PKG_NAME").unwrap().replace("-", "_"))));
-    bindings.write_to_file(out_path.join("parsing_rs.h"));
+    bindings.write_to_file(out_path.join("parser_rs.h"));
 }
