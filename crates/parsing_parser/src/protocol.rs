4dde4270d71f6d2dc7611ce88814c00aa9cc5b61
Saigyouji Yuyuko <liyifan@bolean.com.cn>
2021-11-23 12:36:36 +0800

    chore: remove repr macro

--- crates/parsing_parser/src/protocol.rs
+++ crates/parsing_parser/src/protocol.rs
@@ -4,7 +4,6 @@ use serde::{Serialize, Deserialize};
 /// ProtocolType旨在用简单结构来表示协议类型
 /// * 协助判断解析出来的packet中各层是什么协议
 /// * 也用于options的stop字段说明该在哪一层停止
-#[repr(C)]
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 pub enum ProtocolType {
     Link(LinkProtocol),
@@ -14,27 +13,23 @@ pub enum ProtocolType {
     Error(ParseError),
 }
 
-#[repr(C)]
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 pub enum LinkProtocol {
     Ethernet,
 }
 
-#[repr(C)]
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 pub enum NetworkProtocol {
     Ipv4,
     Ipv6,
 }
 
-#[repr(C)]
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 pub enum TransportProtocol {
     Tcp,
     Udp,
 }
 
-#[repr(C)]
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 pub enum ApplicationProtocol {
     ModbusReq,
@@ -52,7 +47,6 @@ pub enum ApplicationProtocol {
     IsoOnTcp,
 }
 
-#[repr(C)]
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash, Serialize, Deserialize)]
 pub enum ApplicationNaiveProtocol {
     Modbus,
